80b44633aeccddddbb036cada8f1ad4ab9f26036
Slavomir <gagliardetto@users.noreply.github.com>
2019-10-18 23:40:52 +0200

    fix example

--- example/main.go
+++ example/main.go
@@ -9,22 +9,27 @@ import (
 )
 
 func main() {
-	rrURLs, err := roundrobin.NewURLs([]*url.URL{
-		{Host: "192.168.33.10"},
-		{Host: "192.168.33.11"},
-		{Host: "192.168.33.12"},
-		{Host: "192.168.33.13"},
+	////
+	rrURLs, err := roundrobin.New([]interface{}{
+		&url.URL{Host: "192.168.33.10"},
+		&url.URL{Host: "192.168.33.11"},
+		&url.URL{Host: "192.168.33.12"},
+		&url.URL{Host: "192.168.33.13"},
 	})
 	if err != nil {
 		panic(err)
 	}
 
 	start := time.Now()
-	for i := 0; i < 1000000; i++ {
-		rrURLs.Next()
+	for i := 0; i < 10; i++ {
+		u := rrURLs.Next()
+		uu := u.(*url.URL)
+		fmt.Println(uu.Host)
+		uu.Host = "modified"
 		//fmt.Println(rrbn.Next())
 	}
 	fmt.Println("took:", time.Now().Sub(start))
+	return
 
 	///
 	rrStrings, err := roundrobin.NewStrings([]string{
